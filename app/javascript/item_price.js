a9942a4923256f7459c63594007f87ad7ede8a32
RealXiaoLin <yukixechan@gmail.com>
2020-10-30 00:04:00 +0900

    販売価格計算機能実装の修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -7,8 +7,17 @@ function price() {
   itemPrice.addEventListener("input", () => {
     const inputValue = itemPrice.value;
     const includedTaxPrice = Math.floor(inputValue * tax);
-    taxPrice.innerHTML = includedTaxPrice;
-    profit.innerHTML = parseInt(inputValue) + parseInt(includedTaxPrice);
+    const finalPrice  = parseInt(inputValue) - parseInt(includedTaxPrice);
+    if (isNaN(includedTaxPrice)){
+      taxPrice.innerHTML = 0;
+    } else {
+      taxPrice.innerHTML = includedTaxPrice;
+    };
+    if (isNaN(finalPrice)){
+      profit.innerHTML = 0;
+    } else {
+      profit.innerHTML = finalPrice;
+    };
   });
 };
 window.addEventListener("load", price);
