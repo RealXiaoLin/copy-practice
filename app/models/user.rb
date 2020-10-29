705c7902a536600dacc6b7e14b9a9f63711a4594
RealXiaoLin <yukixechan@gmail.com>
2020-10-29 13:28:28 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+    VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+    validates :password, format: {with: VALID_PASSWORD_REGEX, message: "Password Include both letters and numbers"}
 
-    validates :birthday, presence: true
-    validates :nickname, presence: true
-
-  with_options presence: true do
+    with_options presence: true do
+    validates :birthday
+    validates :nickname
     validates :first_name,format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Full-width characters"}
     validates :last_name,format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Full-width characters"}
     validates :kana_first_name,format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
