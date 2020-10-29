ed35376ddde8850c9b193a0f8656cba28534cf75
RealXiaoLin <yukixechan@gmail.com>
2020-10-29 15:15:30 +0900

    アクティブハッシュバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ApplicationRecord
+  validates :title, :description, :image, :category, :condition, :delivery_fee, :shipping_location, :shipment_date, presence: true
+  validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }
+  validates :delivery_fee_id, numericality: { other_than: 1 }
+  validates :shipping_location_id, numericality: { other_than: 1 }
+  validates :shipment_date_id, numericality: { other_than: 1 }
+
+
   belongs_to :user
   has_one_attached :image
   belongs_to_active_hash :category
