ed8f7b528919eeab6154caa579718bfcff4615e9
RealXiaoLin <yukixechan@gmail.com>
2020-10-29 14:55:41 +0900

    アクティブハッシュ導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :delivery_fee
+  belongs_to_active_hash :shipping_location
+  belongs_to_active_hash :shipment_date
 end
