a55f7ec6ddb40d84aa7bd2875b293770f4c693a6
RealXiaoLin <yukixechan@gmail.com>
2020-10-29 21:13:31 +0900

    itemモデル単体テストコード実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,5 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  validates :title, :description, :image, :category, :condition, :delivery_fee, :shipping_location, :shipment_date, presence: true
-  validates :category_id, numericality: { other_than: 1 }
-  validates :condition_id, numericality: { other_than: 1 }
-  validates :delivery_fee_id, numericality: { other_than: 1 }
-  validates :shipping_location_id, numericality: { other_than: 1 }
-  validates :shipment_date_id, numericality: { other_than: 1 }
-
-
   belongs_to :user
   has_one_attached :image
   belongs_to_active_hash :category
@@ -15,4 +7,15 @@ class Item < ApplicationRecord
   belongs_to_active_hash :delivery_fee
   belongs_to_active_hash :shipping_location
   belongs_to_active_hash :shipment_date
+
+  with_options presence: true do
+    validates :title, :description, :image
+    validates :category_id, numericality: { other_than: 1 }
+    validates :condition_id, numericality: { other_than: 1 }
+    validates :delivery_fee_id, numericality: { other_than: 1 }
+    validates :shipping_location_id, numericality: { other_than: 1 }
+    validates :shipment_date_id, numericality: { other_than: 1 }
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}, format: { with: /\A[0-9]{1,7}\z/}
+  end
+
 end
