97da3f5831fdeb81d2f360a250c8a831472d6487
RealXiaoLin <yukixechan@gmail.com>
2020-10-29 16:25:46 +0900

    商品の情報をテーブルに保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,9 +2,21 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      redirect_to new_item_path(@item)
+    end
+  end
 
   private
-  def message_params
-    params.require(:item).permit(:title, :description, :image, :category_id, :condition_id, :delivery_fee_id, :shipping_location_id, :shipment_date_id).merge(user_id: current_user.id)
+  def item_params
+    params.require(:item).permit(:title, :description, :image, :category_id, :condition_id, :delivery_fee_id, :shipping_location_id, :shipment_date_id, :price).merge(user_id: current_user.id)
   end
 end
