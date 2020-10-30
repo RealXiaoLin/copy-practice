7e7696df86e39eb089874541a454c009f6d2e19f
RealXiaoLin <yukixechan@gmail.com>
2020-10-30 19:46:20 +0900

    ActiveHashバリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,12 +10,13 @@ class Item < ApplicationRecord
 
   with_options presence: true do
     validates :title, :description, :image
-    validates :category_id, numericality: { other_than: 1 }
-    validates :condition_id, numericality: { other_than: 1 }
-    validates :delivery_fee_id, numericality: { other_than: 1 }
-    validates :shipping_location_id, numericality: { other_than: 1 }
-    validates :shipment_date_id, numericality: { other_than: 1 }
     validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}, format: { with: /\A[0-9]{1,7}\z/}
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :condition_id
+      validates :delivery_fee_id
+      validates :shipping_location_id
+      validates :shipment_date_id
+    end
   end
-
 end
