65b986acb26fa0cb1a50e3e795424182fb9b6ba9
RealXiaoLin <yukixechan@gmail.com>
2020-10-31 14:48:50 +0900

    記述ミスの修正

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,7 @@
 class Address < ApplicationRecord
   belongs_to :order
 
-  with_optino presence: ture do
+  with_optino presence: true do
     varidates :zip_code, :city, :region_id, :building, :house_number, :phone_number
   end
 end
