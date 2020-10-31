fd55c2f36f76556afe84a2e2560053de13236026
RealXiaoLin <yukixechan@gmail.com>
2020-10-31 15:02:00 +0900

    記述ミスの修正

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,6 +2,6 @@ class Address < ApplicationRecord
   belongs_to :order
 
   with_options presence: true do
-    varidates :zip_code, :city, :region_id, :building, :house_number, :phone_number
+    validates :zip_code, :city, :region_id, :building, :house_number, :phone_number
   end
 end
