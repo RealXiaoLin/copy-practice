2ab02af03f8e2769667b9641fc69a3795c13dc6f
RealXiaoLin <yukixechan@gmail.com>
2020-11-01 20:43:48 +0900

    order_addressモデル追加（Formオブジェクトパターン）

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,10 +3,6 @@ class Address < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :region
 
-
   belongs_to :order
 
-  with_options presence: true do
-    validates :zip_code, :city, :region_id, :building, :house_number, :phone_number
-  end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,28 @@
-class OrderAddress < ApplicationRecord
+class OrderAddress
 
 include ActiveModel::Model
-attr_accessor :zip_code, :city, :region_id, :building, :house_number, :phone_number, :item_id, :user_id
+attr_accessor :zip_code, :city, :region_id, :building, :house_number, :phone_number, :item_id, :user_id, :token
+
+# addressモデルのバリデーション
+with_options presence: true do
+  validates :zip_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
+  validates :region_id, numericality: { other_than: 1 } # 都道府県
+  validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } # 市区町村
+  validates :house_number # 番地
+  validates :building # 建物名
+  validates :phone_number, format: { with: /\A0[0-9]{10,11}\z/ } # 電話番号
+  # orderモデルのバリデーション
+  validates :user_id
+  validates :item_id
+  # tokenのバリデーション
+  validates :token
+
+end
+
+
+def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(zip_code: zip_code, city: city, region_id: region_id, house_number: house_number, building: building, phone_number: phone_number, order_id: order.id)
+  end
 
 end
