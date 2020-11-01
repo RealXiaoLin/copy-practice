68a1ae2f2753f56e84a9b4d892ea001fae8bd226
RealXiaoLin <yukixechan@gmail.com>
2020-11-01 15:25:43 +0900

    addressモデル作成

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,9 @@
 class Address < ApplicationRecord
+  attr_accessor :token
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :region
+
+
   belongs_to :order
 
   with_options presence: true do
