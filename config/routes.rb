ded7cd7b230e5d5d465aea6dcc7cb7ed18dcb5d6
RealXiaoLin <yukixechan@gmail.com>
2020-11-03 20:16:02 +0900

    不要なカード情報登録のルーティングを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  get 'users/update'
-  get 'cards/new'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "items#index"
   resources :items do
     resources :orders, only: [:index, :create]
   end
-  resources :users, only: [:show, :edit, :update]
-  resources :cards, only: [:new, :create]
+  resources :users, only: [:show]
+
+  # カード登録機能オミット中
+  # resources :cards, only: [:new, :create]
 end
