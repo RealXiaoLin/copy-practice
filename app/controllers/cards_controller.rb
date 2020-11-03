308eac7a0cecf65ba4fe46cfe95947485d73d458
RealXiaoLin <yukixechan@gmail.com>
2020-11-03 19:23:17 +0900

    Cardコントローラー修正

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,6 @@
 class CardsController < ApplicationController
   def new
+    @card = Card.new
   end
 
   def create
@@ -9,15 +10,17 @@ class CardsController < ApplicationController
     card: params[:token] # 登録しようとしているカード情報
     )
 
-    card = Card.new( # トークン化されたカード情報を保存する
+    @card = Card.new( # トークン化されたカード情報を保存する
       card_token: params[:token], # カードトークン
       customer_token: customer.id, # 顧客トークン
       user_id: current_user.id # ログインしているユーザー
     )
-    if card.save
+    binding.pry
+    if @card.valid?
+      @card.save
       redirect_to root_path
     else
-      redirect_to new_card_path
+      render :new
     end
   end
 
