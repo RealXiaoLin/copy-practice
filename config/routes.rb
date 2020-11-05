10ac6372e99c78c1239dfee0dbb23dd763aafc11
RealXiaoLin <yukixechan@gmail.com>
2020-11-05 10:26:47 +0900

    messageコントローラーに係るルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
+  get 'messages/new'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "items#index"
+  get '/categories/search', to: 'categories#search'
   resources :items do
     resources :orders, only: [:index, :create]
   end
   resources :users, only: [:show]
   resources :categories, only:[:index]
-  get '/categories/search', to: 'categories#search'
+  resources :messages, only: [:create]
   # カード登録機能オミット中
   # resources :cards, only: [:new, :create]
 end
